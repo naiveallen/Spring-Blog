0d6b3c57b26fb6e49554d64cf6ba0b9fc4d65f3c
naiveallen <pyyuanye@gmail.com>
2019-09-15 19:23:10 -0700

    Request log function

--- src/main/java/com/allen/blog/aspect/LogAspect.java
+++ src/main/java/com/allen/blog/aspect/LogAspect.java
@@ -1,12 +1,48 @@
 package com.allen.blog.aspect;
 
-import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Aspect
 @Component
 public class LogAspect {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @Pointcut("execution(* com.allen.blog.controller.*.*(..))")
+    public void log() {}
+
+    @Before("log()")
+    public void doBefore(JoinPoint joinPoint) {
+        // 从RequestContextHolder中获取ServletRequestAttributes
+        // 然后从ServletRequestAttributes中 获取request
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        String url = request.getRequestURL().toString();
+        String ip = request.getRemoteAddr();
+        // 切入点的信息在 joinPoint中
+        String classMethod = joinPoint.getSignature().getDeclaringTypeName()
+                + "." + joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
+        RequestLog requestLog = new RequestLog(url, ip, classMethod, args);
+
+        logger.info("[REQUEST]: " + requestLog);
+    }
+
+    @After("log()")
+    public void doAfter() { }
+
+    @AfterReturning(pointcut = "log()", returning = "result")
+    public void doAfterReturn(Object result) {
+        logger.info("[RESULT]: " + result);
+    }
 
 }
--- src/main/java/com/allen/blog/controller/BlogController.java
+++ src/main/java/com/allen/blog/controller/BlogController.java
@@ -1,15 +1,15 @@
 package com.allen.blog.controller;
 
-import com.allen.blog.exception.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @Controller
 public class BlogController {
 
-    @GetMapping("/")
-    public String index() {
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable("id") String id, @PathVariable("name") String name) {
 
         return "index";
     }
