f3672b8bbacdd9028a05a13a405cb8415f0e6d4e
naiveallen <pyyuanye@gmail.com>
2019-09-16 10:10:42 -0700

    add static resources and templates

--- src/main/java/com/allen/blog/controller/BlogController.java
+++ src/main/java/com/allen/blog/controller/BlogController.java
@@ -2,15 +2,13 @@ package com.allen.blog.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 
 @Controller
 public class BlogController {
 
-    @GetMapping("/{id}/{name}")
-    public String index(@PathVariable("id") String id, @PathVariable("name") String name) {
-
+    @GetMapping("/")
+    public String index() {
         return "index";
     }
 
